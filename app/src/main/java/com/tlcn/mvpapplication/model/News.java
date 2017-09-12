ac95e7d52a279ca136aa8779c18b62c1aeaf5193
hung <tskiller12345@gmail.com>
2017-09-12 22:37:40 +0700

    MVP favourite fragment

--- app/src/main/java/com/tlcn/mvpapplication/model/News.java
+++ app/src/main/java/com/tlcn/mvpapplication/model/News.java
@@ -16,10 +16,65 @@ public class News {
     @SerializedName("title")
     @Expose
     private String title;
+    @SerializedName("rating")
+    @Expose
+    private float rating;
     @SerializedName("created")
     @Expose
     private Date created;
     @SerializedName("description")
     @Expose
     private String description;
+
+    public News(){
+
+    }
+
+    public News(String id, String title, float rating, Date created, String description) {
+        this.id = id;
+        this.title = title;
+        this.rating = rating;
+        this.created = created;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- app/src/main/java/com/tlcn/mvpapplication/mvp/Main/presenter/FavouritePresenter.java
+++ app/src/main/java/com/tlcn/mvpapplication/mvp/Main/presenter/FavouritePresenter.java
@@ -4,6 +4,7 @@ import com.tlcn.mvpapplication.base.BasePresenter;
 import com.tlcn.mvpapplication.model.News;
 import com.tlcn.mvpapplication.mvp.Main.view.IFavouriteView;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +20,9 @@ public class FavouritePresenter extends BasePresenter implements IFavouritePrese
         super.attachView(view);
     }
 
+    public IFavouriteView getView() {
+        return (IFavouriteView) getIView();
+    }
     @Override
     public void onCreate() {
         super.onCreate();
@@ -26,9 +30,20 @@ public class FavouritePresenter extends BasePresenter implements IFavouritePrese
         news = new News();
     }
 
+    public List<News> getListNewsResult(){
+        return list;
+    }
+
+    public News getNewsInfoResult(String idNews){
+
+        return news;
+    }
+
     @Override
     public void getNewsInfo(String idNews) {
-
+        getView().showLoading();
+        news = new News("1","Kẹt xe tại ngã tư hàng xanh",4,new Date(2017,9,13),"Kẹt xe rất cao");
+        getView().hideLoading();
     }
 
     @Override
