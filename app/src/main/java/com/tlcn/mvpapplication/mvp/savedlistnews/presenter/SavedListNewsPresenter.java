adac3c4b7a4114337ab25724b64b7e41cee91891
Hung <tskiller12345@gmail.com>
2017-10-08 23:37:04 +0700

    save ppresenter

--- app/src/main/java/com/tlcn/mvpapplication/mvp/savedlistnews/presenter/SavedListNewsPresenter.java
+++ app/src/main/java/com/tlcn/mvpapplication/mvp/savedlistnews/presenter/SavedListNewsPresenter.java
@@ -1,8 +1,14 @@
 package com.tlcn.mvpapplication.mvp.savedlistnews.presenter;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.tlcn.mvpapplication.base.BasePresenter;
 import com.tlcn.mvpapplication.model.Locations;
 import com.tlcn.mvpapplication.mvp.savedlistnews.view.ISavedListNewsView;
+import com.tlcn.mvpapplication.utils.KeyUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +20,9 @@ import java.util.List;
 public class SavedListNewsPresenter extends BasePresenter implements ISavedListNewsPresenter {
 
     private List<Locations> list;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mLocationReference;
+    private DatabaseReference mSaveReference;
 
     public void attachView(ISavedListNewsView view) {
         super.attachView(view);
@@ -27,6 +36,9 @@ public class SavedListNewsPresenter extends BasePresenter implements ISavedListN
     public void onCreate() {
         super.onCreate();
         list = new ArrayList<>();
+        mDatabase = FirebaseDatabase.getInstance();
+        mLocationReference = mDatabase.getReference().child(KeyUtils.LOCATIONS);
+        mSaveReference = mDatabase.getReference().child(KeyUtils.SAVE);
     }
 
 
@@ -34,7 +46,20 @@ public class SavedListNewsPresenter extends BasePresenter implements ISavedListN
     public void getSavedListLocation() {
         list.clear();
         getView().showLoading();
+        mSaveReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot data : dataSnapshot.getChildren()){
+                    String location_id = data.child("location_id").getValue().toString();
 
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         getView().onGetSavedListLocationSuccess(list);
         getView().hideLoading();
     }
