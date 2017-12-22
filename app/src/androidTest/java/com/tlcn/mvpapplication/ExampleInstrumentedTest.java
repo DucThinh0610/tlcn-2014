e2e200a44c36ae8350ace2b41ca4f966e225fa06
ducthinh0610 <ducthinh0610@gmail.com>
2017-12-23 01:27:00 +0700

    test

--- app/src/androidTest/java/com/tlcn/mvpapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tlcn/mvpapplication/ExampleInstrumentedTest.java
@@ -2,8 +2,24 @@ package com.tlcn.mvpapplication;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.intent.IntentStubberRegistry;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
+import com.robotium.solo.Solo;
+import com.tlcn.mvpapplication.mvp.chart.ChartActivity;
+import com.tlcn.mvpapplication.mvp.chooselocation.view.ChooseLocationView;
+import com.tlcn.mvpapplication.mvp.main.view.MainActivity;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +32,50 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    private Solo solo;
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-        assertEquals("com.tlcn.mvpapplication", appContext.getPackageName());
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), mainActivityActivityTestRule.getActivity());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testButtonKhac() throws Exception {
+        solo.unlockScreen();
+        solo.sleep(1000);
+        solo.clickOnView(solo.getView(me.riddhimanadib.library.R.id.linearLayoutBar2));
+        solo.sleep(1000);
+        solo.clickOnView(solo.getView(R.id.tv_add));
+        solo.sleep(1000);
+        LinearLayout linearLayout = (LinearLayout) solo.getCurrentActivity().findViewById(R.id.lnl_other);
+        assertEquals("Button [Khac] is not visible", linearLayout.getVisibility(), View.VISIBLE);
+    }
+
+    @Test
+    public void uploadNoImage() throws Exception{
+        solo.sleep(1000);
+        solo.clickOnView(solo.getView(me.riddhimanadib.library.R.id.linearLayoutBar4));
+        solo.sleep(1000);
+        solo.clickOnView(solo.getView(R.id.btn_send));
+        TextView toast = (TextView) solo.getView(android.R.id.message);
+        solo.sleep(1000);
+        assertEquals("toast is not showing", toast.getText().toString(), "Bạn cần đăng kèm hình ảnh!");
+    }
+
+    @Test
+    public void openActivityChooseLocation() throws Exception{
+        solo.clickOnView(solo.getView(me.riddhimanadib.library.R.id.linearLayoutBar4));
+        solo.sleep(1000);
+        solo.clickOnView(solo.getView(R.id.rdb_other));
+        solo.sleep(1000);
+        solo.waitForActivity(ChooseLocationView.class);
+        solo.sleep(1000);
+        solo.assertCurrentActivity("Launch choose location activity", ChartActivity.class);
     }
 }
