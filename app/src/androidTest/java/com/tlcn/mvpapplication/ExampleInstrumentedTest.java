7cda418a29b11b79cb9ec7fb85c54292b7fc158c
hung <tskiller12345@gmail.com>
2017-12-28 13:51:34 +0700

    update

--- app/src/androidTest/java/com/tlcn/mvpapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tlcn/mvpapplication/ExampleInstrumentedTest.java
@@ -1,29 +1,23 @@
 package com.tlcn.mvpapplication;
 
-import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.test.runner.intent.IntentStubberRegistry;
-import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.robotium.solo.Solo;
-import com.tlcn.mvpapplication.mvp.chart.ChartActivity;
 import com.tlcn.mvpapplication.mvp.chooselocation.view.ChooseLocationView;
 import com.tlcn.mvpapplication.mvp.main.view.MainActivity;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -48,34 +42,34 @@ public class ExampleInstrumentedTest {
     @Test
     public void testButtonKhac() throws Exception {
         solo.unlockScreen();
-        solo.sleep(1000);
+        solo.sleep(2000);
         solo.clickOnView(solo.getView(me.riddhimanadib.library.R.id.linearLayoutBar2));
-        solo.sleep(1000);
+        solo.sleep(2000);
         solo.clickOnView(solo.getView(R.id.tv_add));
-        solo.sleep(1000);
+        solo.sleep(2000);
         LinearLayout linearLayout = (LinearLayout) solo.getCurrentActivity().findViewById(R.id.lnl_other);
         assertEquals("Button [Khac] is not visible", linearLayout.getVisibility(), View.VISIBLE);
     }
 
     @Test
     public void uploadNoImage() throws Exception{
-        solo.sleep(1000);
+        solo.sleep(2000);
         solo.clickOnView(solo.getView(me.riddhimanadib.library.R.id.linearLayoutBar4));
-        solo.sleep(1000);
+        solo.sleep(2000);
         solo.clickOnView(solo.getView(R.id.btn_send));
         TextView toast = (TextView) solo.getView(android.R.id.message);
-        solo.sleep(1000);
+        solo.sleep(2000);
         assertEquals("toast is not showing", toast.getText().toString(), "Bạn cần đăng kèm hình ảnh!");
     }
 
     @Test
     public void openActivityChooseLocation() throws Exception{
         solo.clickOnView(solo.getView(me.riddhimanadib.library.R.id.linearLayoutBar4));
-        solo.sleep(1000);
+        solo.sleep(2000);
         solo.clickOnView(solo.getView(R.id.rdb_other));
-        solo.sleep(1000);
+        solo.sleep(2000);
         solo.waitForActivity(ChooseLocationView.class);
-        solo.sleep(1000);
-        solo.assertCurrentActivity("Launch choose location activity", ChartActivity.class);
+        solo.sleep(2000);
+        solo.assertCurrentActivity("Launch choose location activity", ChooseLocationView.class);
     }
 }
